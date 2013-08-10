fb6f9c12237bdecfbb66865ba6fddd09ea56f568
Matt DuVall <mduvall89@gmail.com>
2013-08-10 10:20:34 -0700

    Tab width taken in to line length

--- tests/lib/rules/maxlen.js
+++ tests/lib/rules/maxlen.js
@@ -30,7 +30,7 @@ vows.describe(RULE_ID).addBatch({
         "should report a violation": function(topic) {
 
             var config = { rules: {} };
-            config.rules[RULE_ID] = [1, 80];
+            config.rules[RULE_ID] = [1, 80, 4];
 
             var messages = eslint.verify(topic, config);
 
@@ -45,13 +45,49 @@ vows.describe(RULE_ID).addBatch({
         "should report a violation": function(topic) {
 
             var config = { rules: {} };
-            config.rules[RULE_ID] = [1, 10];
+            config.rules[RULE_ID] = [1, 10, 4];
 
             var messages = eslint.verify(topic, config);
 
             assert.equal(messages.length, 1);
             assert.equal(messages[0].ruleId, RULE_ID);
             assert.equal(messages[0].message, "Line 0 exceeds the maximum line length of 10.");
+            assert.include(messages[0].node.type, "Program");
+        }
+    },
+
+    "when evaluating '\t\t\tvar i = 1;'": {
+
+        topic: '\t\t\tvar i = 1;',
+
+        "should report a violation": function(topic) {
+
+            var config = { rules: {} };
+            config.rules[RULE_ID] = [1, 15, 4];
+
+            var messages = eslint.verify(topic, config);
+
+            assert.equal(messages.length, 1);
+            assert.equal(messages[0].ruleId, RULE_ID);
+            assert.equal(messages[0].message, "Line 0 exceeds the maximum line length of 15.");
+            assert.include(messages[0].node.type, "Program");
+        }
+    },
+
+    "when evaluating '\t\t\tvar i = 1;\n\t\t\tvar j = 1;'": {
+
+        topic: '\t\t\tvar i = 1;\n\t\t\tvar j = 1;',
+
+        "should report a violation": function(topic) {
+
+            var config = { rules: {} };
+            config.rules[RULE_ID] = [1, 15, 4];
+
+            var messages = eslint.verify(topic, config);
+
+            assert.equal(messages.length, 2);
+            assert.equal(messages[0].ruleId, RULE_ID);
+            assert.equal(messages[0].message, "Line 0 exceeds the maximum line length of 15.");
             assert.include(messages[0].node.type, "Program");
         }
     }
